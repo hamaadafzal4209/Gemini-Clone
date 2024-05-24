aaf207423978f0968f430de2fb071b1f4a6dcc41
FrontendFlair <hamaadafzal404@gmail.com>
2024-05-24 03:58:26 -0700

    Add gemini API and Test it properly

---
+++ src/context/Context.jsx
@@ -0,0 +1,42 @@
+import { createContext, useState } from "react";
+import run from "../config/gemini";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [prevPrompts, setPrevPrompts] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
+
+  const onSent = async (prompt) => {
+
+    setResultData("");
+    setLoading(true);
+    setShowResult(true);
+    await run(input);
+  };
+
+  const contextValue = {
+    prevPrompts,
+    setPrevPrompts,
+    onSent,
+    recentPrompt,
+    setRecentPrompt,
+    showResult,
+    loading,
+    resultData,
+    input,
+    setInput,
+  };
+
+  return (
+    <Context.Provider value={contextValue}>
+      {props.children}
+    </Context.Provider>
+  );
+};
+
+export default ContextProvider;
