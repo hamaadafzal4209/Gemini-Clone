7b1109a7acceb255d16cb6fbfa33004ca75ca5fa
FrontendFlair <hamaadafzal404@gmail.com>
2024-05-24 00:00:50 -0700

    complete UI for main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import Sidebar from './Components/Sidebar/Sidebar'
-import Main from './Components/Main/Main'
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Main from "./Components/Main/Main";
 
 function App() {
   return (
-    <div>
-      <Sidebar/>
-      <Main/>
+    <div style={{ minHeight: "100vh", display: "flex" }}>
+      <Sidebar />
+      <Main />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
