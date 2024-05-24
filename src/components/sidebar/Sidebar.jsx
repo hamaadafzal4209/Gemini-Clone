7f9323497305e3381e8a12102e2676a5589e341f
FrontendFlair <hamaadafzal404@gmail.com>
2024-05-23 22:10:26 -0700

    complete sidebar U

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
 import './Sidebar.css'
+import {assets} from '../../assets/assets'
 
 function Sidebar() {
   return (
-    <div>
-      
+    <div className='sidebar'>
+      <div className="top">
+        <img className='menu' src={assets.menu_icon} alt="" />
+        <div className="new-chat">
+          <img src={assets.plus_icon} alt="" />
+          <p>New Chat</p>
+        </div>
+        <div className="recent">
+          <p className="recent-title">Recent</p>
+          <div className="recent-entry">
+            <img src={assets.message_icon} alt="" />
+            <p>What is React...</p>
+          </div>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+          <img src={assets.question_icon} alt="" />
+          <p>Help</p>
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.history_icon} alt="" />
+          <p>Activity</p>
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.setting_icon} alt="" />
+          <p>Setting</p>
+        </div>
+      </div>
     </div>
   )
 }
-import './Sidebar.css'
 
 export default Sidebar
